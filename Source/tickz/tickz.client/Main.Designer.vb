734aa3ed5afa808ca52bf88b8c0fb76ebb74a73f
Paulmichael Blasucci <pblasucci@gmail.com>
2014-07-10 11:10:44 -0500

    changed tickz.client to WinForms

---
+++ Source/tickz/tickz.client/Main.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+    Me.StockLabel = New System.Windows.Forms.Label()
+    Me.StockComboBox = New System.Windows.Forms.ComboBox()
+    Me.SuspendLayout
+    '
+    'StockLabel
+    '
+    Me.StockLabel.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+            Or System.Windows.Forms.AnchorStyles.Left)  _
+            Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+    Me.StockLabel.BackColor = System.Drawing.Color.White
+    Me.StockLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+    Me.StockLabel.Font = New System.Drawing.Font("Consolas", 32!, System.Drawing.FontStyle.Bold)
+    Me.StockLabel.ForeColor = System.Drawing.Color.White
+    Me.StockLabel.Location = New System.Drawing.Point(12, 107)
+    Me.StockLabel.Margin = New System.Windows.Forms.Padding(3, 10, 3, 10)
+    Me.StockLabel.Name = "StockLabel"
+    Me.StockLabel.Size = New System.Drawing.Size(434, 98)
+    Me.StockLabel.TabIndex = 0
+    Me.StockLabel.Text = "0.0"
+    Me.StockLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight
+    Me.StockLabel.UseMnemonic = false
+    '
+    'StockComboBox
+    '
+    Me.StockComboBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left)  _
+            Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+    Me.StockComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+    Me.StockComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+    Me.StockComboBox.Font = New System.Drawing.Font("Consolas", 28!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+    Me.StockComboBox.Items.AddRange(New Object() {"AAPL", "BBRY", "GOOG", "MSFT", "YHOO"})
+    Me.StockComboBox.Location = New System.Drawing.Point(12, 12)
+    Me.StockComboBox.MaxDropDownItems = 5
+    Me.StockComboBox.Name = "StockComboBox"
+    Me.StockComboBox.Size = New System.Drawing.Size(434, 74)
+    Me.StockComboBox.Sorted = true
+    Me.StockComboBox.TabIndex = 1
+    '
+    'Main
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(9!, 20!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.BackColor = System.Drawing.Color.Linen
+    Me.ClientSize = New System.Drawing.Size(458, 224)
+    Me.Controls.Add(Me.StockComboBox)
+    Me.Controls.Add(Me.StockLabel)
+    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+    Me.MaximizeBox = false
+    Me.MinimizeBox = false
+    Me.Name = "Main"
+    Me.Text = "tickz"
+    Me.ResumeLayout(false)
+
+End Sub
+    Friend WithEvents StockLabel As System.Windows.Forms.Label
+    Friend WithEvents StockComboBox As System.Windows.Forms.ComboBox
+End Class
---
+++ Source/tickz/tickz.client/Main.vb
@@ -0,0 +1,70 @@
+﻿Imports fszmq
+Imports System.Collections.Concurrent
+Imports System.Diagnostics
+Imports System.Text
+Imports System.Threading
+
+Public Class Main
+
+  Private Shared TopicQueue   As New ConcurrentQueue(Of String)()
+  Private Shared TokenSource  As New CancellationTokenSource()
+
+  Delegate Sub UpdateValue(ByVal value As Double)
+  Private DoUpdateValue As UpdateValue = New UpdateValue(AddressOf UpdateValueLabel)
+  Private Sub UpdateValueLabel(ByVal value As Double)
+    Dim Previous = CDbl(StockLabel.Text)
+    StockLabel.BackColor = CType(IIf(value > Previous, Color.Green, Color.Red), Color)
+    StockLabel.Text = String.Format("{0:F3}", value)
+    Previous = value
+  End Sub
+
+  Private Sub PollTopic(ByVal o As Object)
+    Dim Token     = CType(o, CancellationToken)
+    Dim Previous  = ""
+    Using Context = New Context() _
+        , Socket  = Context.Sub()
+      Socket.Connect("tcp://localhost:9003")
+      While Not Token.IsCancellationRequested
+        Dim Topic As String = ""
+        If TopicQueue.TryDequeue(Topic) Then
+          If Not String.IsNullOrWhiteSpace(Previous) Then
+            Socket.Unsubscribe( { Encoding.UTF8.GetBytes(Previous) } )
+          End If
+          Socket.Subscribe  ( { Encoding.UTF8.GetBytes(Topic   ) } )
+          Previous = Topic
+        End If
+        Dim Message As Byte()() = {} 
+        If Socket.TryGetInput(1000, Message) AndAlso Message.Length = 3 Then
+          Dim Symbol  = Encoding.UTF8.GetString(Message(0))
+          Dim Value   = BitConverter.ToDouble(Message(2), 0)
+          Debug.WriteLine("{0} = {1:F3}", Symbol, Value)
+          If Not Token.IsCancellationRequested Then
+            StockLabel.Invoke(DoUpdateValue,CObj(value))
+          End If
+        End If
+      End While
+    End Using
+    Debug.WriteLine("Proxy Exited Cleanly")
+  End Sub
+
+  Private Proxy As New Thread(AddressOf PollTopic)
+
+  Private Sub Main_Load(ByVal sender As Object, ByVal e As EventArgs) _ 
+    Handles Me.Load
+    Proxy.Start(TokenSource.Token)
+  End Sub
+
+  Private Sub Main_FormClosing(ByVal sender As Object, Byval e As FormClosingEventArgs) _ 
+    Handles Me.FormClosing
+    TokenSource.Cancel()
+    Proxy.Join()
+  End Sub
+
+
+  Private Sub StockComboBox_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) _ 
+    Handles StockComboBox.SelectedIndexChanged
+    StockLabel.BackColor = Color.White
+    TopicQueue.Enqueue(CStr(StockComboBox.SelectedItem))
+  End Sub
+
+End Class
