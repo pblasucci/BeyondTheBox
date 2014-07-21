e90fb643758da302dda12d1979c40d7e04242fe3
Paulmichael Blasucci <pblasucci@gmail.com>
2014-07-21 01:34:32 -0500

    add web-based dealz skeleton

--- Source/dealz/dealz.client/Views.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace dealz.client.Views
-
-open FsXaml
-
-type ChatzView  = XAML<"ChatzView.xaml">
-type TickzView  = XAML<"TickzView.xaml">
-type ValuzView  = XAML<"ValuzView.xaml">
-type MainWindow = XAML<"MainWindow.xaml">
---
+++ Source/dealz/dealz/Models/Car.fs
@@ -0,0 +1,9 @@
+namespace dealz.Models
+
+open Newtonsoft.Json
+
+[<CLIMutable>]
+type Car = {
+    Make : string
+    Model : string
+}
