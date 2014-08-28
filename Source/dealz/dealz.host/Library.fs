b90f61aad9dc074b8e8c2f519b4ae92cc0dbe61f
Paulmichael Blasucci <pblasucci@gmail.com>
2014-08-28 16:47:24 -0400

    added comments to dealz.host code

--- Source/dealz/dealz.host/Library.fs
+++ Source/dealz/dealz.host/Library.fs
@@ -1,5 +1,7 @@
 ﻿namespace dealz.Host
 
+(* aliases and units-of-measure to make code read better *)
+
 type date = System.DateTime
 type time = System.TimeSpan
 
@@ -8,15 +10,17 @@ type fetch<'R> = AsyncReplyChannel<'R>
 
 type [<Measure>] msec
 
-
+/// helper functions for string manipulation
 [<RequireQualifiedAccess>]
 module String =
 
   open System
-  
+
+  /// splits string based on presence of a seperator
   let split (seperator :string) (value :string) =
     value.Split (seperator.ToCharArray(),StringSplitOptions.RemoveEmptyEntries)
 
+/// helper functions to make OWIN more "F# friendly"
 [<RequireQualifiedAccess>]
 module Owin =
   
@@ -26,74 +30,97 @@ module Owin =
   open Microsoft.AspNet.SignalR
   open System
 
+  /// enables hosting of static files from disk
   let useStaticFiles (app :IAppBuilder) = app.UseStaticFiles ()
-  
+
+  /// exposes SignalR at the given path
   let mapSignalR<'T when 'T :> PersistentConnection> path (app :IAppBuilder) = app.MapSignalR<'T> path
 
+  /// kick-starts the OWIN web hosting process
   let start url config = WebApp.Start (string url,Action<IAppBuilder>(config))
 
+  /// add a custom dependency-injection resolver for the given type
   let addResolver<'T> resolve = GlobalHost.DependencyResolver.Register (typeof<'T>,(fun () -> box<'T> <| resolve())) 
 
+  /// gets an instance of the given type from the inversion-of-control container
   let resolve<'T> () = GlobalHost.DependencyResolver.Resolve<'T> () 
 
 
+(* Domain Types *)
+
+/// represents a message in the chatz service
 type Memo =
   { Sender  :string
     Message :string }
 
+/// represents a discrete point-in-time stock price in the tickz service
 type Tick =
   { Stock :string
     Stamp :date 
     Price :float }
 
+/// represents an trade order in the valuz service
 type Order =
   { Stock   :string 
     Action  :BuySell
     Price   :float }
 and BuySell = Buy | Sell
 
+/// represents the result of pricing a trade order via the valuz service
 type Calc =
   { Value :float }
 
+/// defines the messages sent, via SignalR, from the client to the web server
 type ClientMsg =
   | Follow of symbol:string
   | Forget of symbol:string
   | Jabber of report:string
   | Reckon of trades:Order[]
 
+/// defines the messages sent, via SignalR, from the web server to the client
 type ServerMsg =
-  | Acknowledged
-  | Failed of error:string
-  | Ticked of Tick
-  | Memoed of Memo
-  | Joined of users:string[]
-  | Solved of Calc
-  | Disconnect
+  | Acknowledged              // sent from web server
+  | Failed of error:string    // sent from web server
+  | Ticked of Tick            // sent from tickz service
+  | Memoed of Memo            // sent from chatz service
+  | Joined of users:string[]  // sent from chatz service
+  | Solved of Calc            // sent from valuz service
+  | Disconnect                // sent from web server
 
+/// provides an API for the tickz, chatz, and valuz services (which is consumed in the web server)
 type IServiceProxy =
   inherit System.IDisposable
-
+  /// start tracking tick data for a stock
   abstract Follow : stock:string  -> unit
+  /// stop tracking tick data fro a stock
   abstract Forget : stock:string  -> unit
+  /// send a chat message to the group
   abstract Jabber : words:string  -> unit
+  /// price a trade order
   abstract Reckon : trade:Order[] -> unit
 
+/// provides a DI-friendly way for services to send messages back to the client (via SignalR)
 type IClientProxy =
   abstract Broadcast : ServerMsg -> unit
 
 
+/// miscellaneous helper functions
 [<AutoOpen>]
 module Library = 
 
   open Newtonsoft.Json
   open System
 
+  /// cleans up any disposable resource
   let inline dispose (o :^o :> IDisposable) = o.Dispose ()
-  
+
+  /// treats Choice2 constructs as either success or failure
   let inline (|Success|Failure|) value = 
     match value with
     | Choice1Of2 value -> Success value
     | Choice2Of2 value -> Failure value
+
+  (* helpers to simplify working with JSON.NET *)
 
   let toJSON      value = JsonConvert.SerializeObject value
   let ofJSON<'T>  value = JsonConvert.DeserializeObject<'T> value 
@@ -105,26 +132,33 @@ module Library =
   let encode = string >> System.Text.Encoding.UTF8.GetBytes
   let decode = System.Text.Encoding.UTF8.GetString
 
+  (* extensions to DateTime for dealing with unix-epoch-derived values *)
+
   type System.DateTime with
     static member UnixEpoch = date(1970,1,1,0,0,0,DateTimeKind.Utc)
     static member inline SinceEpoch offset =
       (offset :^a when ^a : (static member op_Explicit : ^a -> float))
       |> float
       |> date.UnixEpoch.AddSeconds 
-  
+   
+  (* operators and extensions for working with MailboxProcessors *)
+
   let inline (-->) (agent :agent<_>) msg = agent.PostAndReply msg
   let inline (<--) (agent :agent<_>) msg = agent.Post         msg
-    
-
+  
   type Microsoft.FSharp.Control.MailboxProcessor<'T> with
     member self.TryReceive (timeout :int<'u>) = self.TryReceive (int timeout)
 
+  (* active recognizers to help with parsing *)
+
+  /// safely decodes UTF8 data into a String
   let (|Str|_|) raw =
     try
       Some (decode raw)
     with
       | _ -> None
     
+  /// safely converts raw data into a Dobule
   let (|Dbl|_|) raw =
     try
       (raw,0)
@@ -133,6 +167,7 @@ module Library =
     with
       | _ -> None
 
+  /// safely converts raw data into a DateTime (via Unix epoch)
   let (|Dts|_|) raw =
     try
       (raw,0)
@@ -142,6 +177,9 @@ module Library =
     with
       | _ -> None
 
+  (* active recognizers to help convert from frames to objects *)
+
+  /// validates framing and builds a Memo instance
   let (|Memo|_|) msg =
     match Array.length msg with
     | 1 ->  match msg.[0] with
@@ -152,6 +190,7 @@ module Library =
             | _                                   -> None
     | _                                           -> None
 
+  /// validates framing and builds a Tick instance
   let (|Tick|_|) msg =
     match Array.length msg with
     | 3 ->  match msg.[0],msg.[1],msg.[2] with
@@ -161,6 +200,7 @@ module Library =
             | _                             -> None
     | _                                     -> None
 
+  /// validates framing and builds a Calc instance
   let (|Calc|_|) msg =
     match Array.length msg with
     | 1 ->  match msg.[0] with
--- Source/dealz/dealz.host/Services.fs
+++ Source/dealz/dealz.host/Services.fs
@@ -5,173 +5,209 @@ open fszmq.Polling
 open System
 open System.Diagnostics
    
+/// sends messges to the client (via SignalR)
 let dispatch message = Owin.resolve<IClientProxy>().Broadcast message
+
+/// logs diagnotic data
 let tracelog message = System.Diagnostics.Debug.WriteLine message
 
+(* chatz service proxy *)
 
 type ChatzMsg =
-  | Jabber of report:string
-  | Escape of signal:fetch<unit>
+  | Jabber of report:string       // send message to group
+  | Escape of signal:fetch<unit>  // shut down the proxy
 
 let chatz context handle tell hear = agent<ChatzMsg>.Start (fun inbox -> 
-  let chatz_req = Context.req context
-  let chatz_sub = Context.sub context
-  
+  // create chatz sockets
+  let chatz_req = Context.req context // send messgage to group (or send keep-alive)
+  let chatz_sub = Context.sub context // receive group broadcast
+  // configure sockets
   Socket.connect    chatz_req tell
   Socket.connect    chatz_sub hear
   Socket.subscribe  chatz_sub [ ""B ] // all topics
-    
+
+  // encapsulate sending a message (or keep-alive) and recving the result
   let exchange msg =
     msg
     |> encode
     |> Socket.send chatz_req
-     
+    // service always replys with a list of connected peers
     chatz_req
     |> Socket.recvAll
     |> Array.map decode
     |> Joined
-    |> dispatch
+    |> dispatch // pass list of peers to client
 
+  // recursively check for input service then client
   let rec loop () = async {
+    // check for input from service
     chatz_sub
     |> tryPollInput 500L<msec>
     |> Option.iter  (function 
-                    | Memo m  ->  dispatch (Memoed m) 
+                    | Memo m  ->  // got a message... pass it to client
+                                  dispatch (Memoed m) 
                     | bytes   ->  bytes
                                   |> sprintf "Invalid Message: %A"
                                   |> tracelog) 
-    
-
+    // check for input from client
     let!  msg = inbox.TryReceive 100<msec>
     match msg with
-    | None                ->  exchange handle
+    | None                ->  // no input... send "keep-alive"
+                              exchange handle
                               return! loop ()
-    | Some (Jabber report) -> exchange (sprintf "%s\u221E%s" handle report)
+    | Some (Jabber report) -> // got message... send to service for broadcast
+                              exchange (sprintf "%s\u221E%s" handle report)
                               return! loop ()
-    | Some (Escape signal) -> dispatch Disconnect
+    | Some (Escape signal) -> // clean up sockets and shut down
+                              dispatch Disconnect
                               [ chatz_req; chatz_sub; ] |> Seq.iter dispose
                               signal.Reply ()
                               return () }
   
   loop ())
 
+(* tickz service proxy *)
 
 type TickzMsg =
-  | Follow of symbol:string
-  | Ignore of symbol:string
-  | Cutoff of signal:fetch<unit>
+  | Follow of symbol:string       // start receiving ticks for symbol
+  | Ignore of symbol:string       // stop receiving ticks for symbol
+  | Cutoff of signal:fetch<unit>  // shut down the proxy
 
 let tickz context address = agent<TickzMsg>.Start (fun inbox -> 
-
+  // create tickz socket
   let tickz_sub = Context.sub context
-  Socket.connect tickz_sub address
+  Socket.connect tickz_sub address  // recvs tick data for active subscriptions
     
+  // recursively check for input from service then client
   let rec loop () = async {
+    // check for input from service
     tickz_sub
     |> tryPollInput 500L<msec>
     |> Option.bind (function 
-                    | Tick t  -> Some (Ticked t) 
+                    | Tick t  -> // got a Tick... pass it to client
+                                 Some (Ticked t) 
                     | _       -> None) 
     |> Option.iter dispatch
-
+    // check for input from client
     let!  msg = inbox.TryReceive 100<msec>
     match msg with
-    | None                  ->  return! loop ()
-    | Some (Follow symbol)  ->  Socket.subscribe tickz_sub [encode symbol]
+    | None                  ->  // no input... nothing to do
                                 return! loop ()
-    | Some (Ignore symbol)  ->  Socket.unsubscribe tickz_sub [encode symbol]
+    | Some (Follow symbol)  ->  // got input... add subscription
+                                Socket.subscribe tickz_sub [encode symbol]
                                 return! loop ()
-    | Some (Cutoff signal)  ->  dispatch Disconnect
+    | Some (Ignore symbol)  ->  // got input... remove subscription
+                                Socket.unsubscribe tickz_sub [encode symbol]
+                                return! loop ()
+    | Some (Cutoff signal)  ->  // clean up sockets and shut down
+                                dispatch Disconnect
                                 dispose tickz_sub
                                 signal.Reply ()
                                 return ()}
   
   loop ())
 
+(* valuz service proxy *)
 
 type ValuzMsg =
   | Reckon of trades:Order[]
   | Cancel of signal:fetch<unit>
 
 let valuz context workerPath workerCount = agent<ValuzMsg>.Start (fun inbox -> 
-  
+  // create valuz sockets
   let valuz_push = Context.push context
   let valuz_pull = Context.pull context
   let valuz_pub  = Context.pub  context
-
-  Socket.bind valuz_push "tcp://*:9004"
-  Socket.bind valuz_pull "tcp://*:9005"
-  Socket.bind valuz_pub  "tcp://*:9006"
+  // configure valuz sockets
+  Socket.bind valuz_push "tcp://*:9004" // sends trade order data to workers
+  Socket.bind valuz_pull "tcp://*:9005" // gets ready signal and final value from workers
+  Socket.bind valuz_pub  "tcp://*:9006" // sends kill signal to workers
    
+  // recursively wait for desired number of workers to indicate readiness
   let rec waitForReady expected actual =
       if expected > actual then
         valuz_pull |> Socket.recv |> ignore
         waitForReady expected (actual + 1)
-    
+  
+  // recursively wait for all workers to finish
   let rec waitForFinish expected values =
     if expected > List.length values
       then  let value = valuz_pull
                         |> Socket.recv
                         |> decode
                         |> float
-            waitForFinish expected (value::values)
-      else  values
-    
+            waitForFinish expected (value::values) // each final value (from worker) is added to all other final values
+      else  values // all workers finished, return aggregate value
+  
+  // convert Order instance to message frames
   let framedOrder { Stock = stock; Action = action; Price = price; } = 
     [|  encode stock
         encode (match action with Buy -> +1 | _ -> -1)
         encode (string price)  |]
    
+  // spawns a new worker process
   let launchWorker () =
     ProcessStartInfo(workerPath,WindowStyle=ProcessWindowStyle.Hidden)
     |> Process.Start
     |> ignore
 
+  // recursively await a trade order
   let rec idle () = async {
     let!  msg = inbox.TryReceive 100<msec>
     match msg with
-    | None                  ->  return! idle ()
-    | Some (Reckon orders)  ->  return! calculate orders
-    | Some (Cancel signal)  ->  
-        dispatch Disconnect
-        [ valuz_push; valuz_pull; valuz_pub; ] |> Seq.iter dispose
-        signal.Reply ()
-        return () }
-
+    | None                  ->  // no input... keep waiting
+                                return! idle ()
+    | Some (Reckon orders)  ->  // got input... switch to 'busy' mode
+                                return! calculate orders
+    | Some (Cancel signal)  ->  // clean up and shut down
+                                dispatch Disconnect
+                                [ valuz_push; valuz_pull; valuz_pub; ] |> Seq.iter dispose
+                                signal.Reply ()
+                                return () }
+  // price out a trade order
   and calculate orders = async {
+    // launch desired number of workers
     for _ in 1 .. workerCount do launchWorker ()
-    waitForReady  workerCount 0
-
+    // wait all workers to be ready
+    waitForReady workerCount 0
+    // convert each order to a frameset and dispatch to all workers
     orders 
     |> Seq.map framedOrder 
     |> Seq.iter (Socket.sendAll valuz_push)
-
+    // aggregate results of each worker
     let values = waitForFinish (Array.length orders) []
+    // shutdown workers
     Socket.send valuz_pub "batch.leave"B
+    // send aggregate value to client
     dispatch  (Solved { Value = List.sum values })
+    // switch back to 'idle' mode
     return! idle () }
   
   idle ())
 
-
+//NOTE: shutting down any service proxy, shuts down the client connection!
 let start handle (chatzTell,chatzHear,tickzLink) workerPath workerCount =
+  // create one context to use with all service sockets
   let context = new Context ()
-
+  // launch service proxies (ie: MailboxProcessors)
   let chatz = chatz context handle chatzTell chatzHear
   let tickz = tickz context tickzLink
   let valuz = valuz context workerPath workerCount
-
+ 
   let binnable : IDisposable list = [context; chatz; tickz; valuz]
 
+  // create facade for services
   { new IServiceProxy with
-      member __.Jabber report = chatz <-- Jabber report
-      member __.Follow symbol = tickz <-- Follow symbol
-      member __.Forget symbol = tickz <-- Ignore symbol
-      member __.Reckon trades = valuz <-- Reckon trades
+      member __.Jabber report = chatz <-- Jabber report // send message to group
+      member __.Follow symbol = tickz <-- Follow symbol // start recving stock data
+      member __.Forget symbol = tickz <-- Ignore symbol // stop recving stock data
+      member __.Reckon trades = valuz <-- Reckon trades // price trade order
 
     interface IDisposable with
       member __.Dispose () =
+        // shut down each service proxy
         chatz --> Escape
         tickz --> Cutoff
         valuz --> Cancel
+        // clean up resources
         binnable |> List.iter dispose }
