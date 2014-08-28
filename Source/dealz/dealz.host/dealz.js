337b4d799672ef75aaee95ab709dba8eb80aa8f2
Paulmichael Blasucci <pblasucci@gmail.com>
2014-08-28 12:19:07 -0400

    dealz gui tweaks

--- Source/dealz/dealz.host/dealz.js
+++ Source/dealz/dealz.host/dealz.js
@@ -1,61 +1,64 @@
-﻿function buildFollowMsg (stock) {
-  var msg = { Case: "Follow", Fields: [ stock ] };
+﻿function buildMessage (tag, fields) {
+  var msg = { Case: tag, Fields: fields };
   return window.JSON.stringify(msg);
+}
+
+function ticked (tick) {
+  $('#tickz_out_' + tick.Stock).val(tick.Price);
+  //TODO: update line chart
 }
 
 $(function () {
   "use strict";
-  var orders = new Array();
-  for (var i = 0, j = 1, k = 2; k < 500; i += 3, j += 3, k += 3) {
-    orders[i] = { Stock: "MSFT", Action: { Case: "Buy", Fields: [] }, Price: 19.997 };
-    orders[j] = { Stock: "APPL", Action: { Case: "Buy", Fields: [] }, Price: 35.765 };
-    orders[k] = { Stock: "GOOG", Action: { Case: "Sell", Fields: [] }, Price: 256.004 };
-  }
-  var trades = { Case: "Reckon", Fields: [orders] };
-  var SMALLTRADE = window.JSON.stringify(trades);
-  var FOLLOWMSFT = window.JSON.stringify({ Case: "Follow", Fields: ["MSFT"] });
-  var FORGETMSFT = window.JSON.stringify({ Case: "Forget", Fields: ["MSFT"] });
+  //var orders = [];
+  //for (var i = 0, j = 1, k = 2; k < 500; i += 3, j += 3, k += 3) {
+  //  orders[i] = { Stock: "MSFT", Action: { Case: "Buy" , Fields: [] }, Price:  19.997 };
+  //  orders[j] = { Stock: "APPL", Action: { Case: "Buy" , Fields: [] }, Price:  35.765 };
+  //  orders[k] = { Stock: "GOOG", Action: { Case: "Sell", Fields: [] }, Price: 256.004 };
+  //}
+  //var SMALLTRADE = buildMessage("Reckon", [ orders ]);
+  
   var c = $.connection('signalr');
   c.received(function (msg) {
     switch (msg.Case) {
       case 'Ticked': // of Tick
-        $('#tickz_out').val(msg.Fields[0].Price);
+        ticked (msg.Fields[0]);
         break;
+      
       case 'Memoed': // of Memo
-        $('#chatz_out').val(msg.Fields[0].Message);
+        //TODO: ???
         break;
+      
       case 'Joined': // of users:string[]
-        $('#users').val(msg.Fields[0]);
+        //TODO: ???
         break;
+      
       case 'Solved': // of Calc
-        $('#valuz_out').val(msg.Fields[0].Value);
+        //TODO: ???
         break;
+      
       case 'Disconnect':
         console.log(msg.Case);
         c.stop();
-        break
+        break;
+
       default:
-        if (msg.Fields.length == 0) {
-          console.log(msg.Case);
-        } else {
-          console.log(msg.Fields[0]);
-        }
+        var info = msg.Case; 
+        if (msg.Fields.length > 0) {
+          info += (": " + msg.Fields[0]);  
+        } 
+        console.log();
         break;
     }
   })
-  .start({ transport: 'longPolling' }, function () {
-    console.log("Starting SignalR");
-  })
+  .start({ transport: 'longPolling' })
   .done(function () {
-    $("#follow").click(function () { c.send(FOLLOWMSFT); });
-    $("#forget").click(function () { c.send(FORGETMSFT); });
-    $("#jabber").click(function () {
-      var inp = $("#jabber_words");
-      var txt = inp.val();
-      var msg = { Case: "Jabber", Fields: [txt] };
-      c.send(window.JSON.stringify(msg));
-      inp.val("");
+    $('#followStock').submit(function () {
+      //TODO: send topic to server
+      //TODO: add stock field to gui
+      //TODO: reset form
+      //TODO: add stock to line chart
+      return false;
     });
-    $("#reckon").click(function () { c.send(SMALLTRADE); });
   });
 });
